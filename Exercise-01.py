d2eeca9e3873b271b38c2eddab17b29429dd524e
AzisHendrawan <azishendrazis12@gmail.com>
2023-03-10 14:26:48 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,15 @@
+
+
+print("""
+Nama : Azis Hendrawan
+NIM  : I0322021
+
+Saya tinggal di Tampir, Musuk, Boyolali. Tempat tinggal saya terletak di pusat
+desa, tepat di depan rumah joglo tampir dan patung kuda. apabila kita dari UNS, maka
+rumah saya berada di sebelah kiri jalan utama musuk dan berada di samping minimarket.
+Di depan rumah saya juga terdapat ruko-ruko kecil yang disewakan serta terdapat 2 buah 
+pohon rambutan dan mangga. olah karena itu, rumah saya memiliki suasane yang ramai.
+
+Harapan saya ketika lulus dari teknik industri yaitu memiliki pekerjaan tetap yang sesuai
+dengan ilmu-ilmu yang sudah saya pelajari di teknik industri. 
+""")
